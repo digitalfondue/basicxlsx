7c833564a592dec88ce5f7e85e3119fabe42d5e4
Sylvain Jermini <sylvain.jermini@syjer.com>
2018-09-21 20:58:03 +0200

    additional api in sheet

--- src/main/java/ch/digitalfondue/basicxlsx/Sheet.java
+++ src/main/java/ch/digitalfondue/basicxlsx/Sheet.java
@@ -2,6 +2,7 @@ package ch.digitalfondue.basicxlsx;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class Sheet {
 
@@ -13,7 +14,25 @@ public class Sheet {
         this.name = name;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public void setCellAt(Cell cell, int row, int column) {
         cells.computeIfAbsent(row, r -> new HashMap<>()).put(column, cell);
+    }
+
+    public void removeCellAt(int row, int column) {
+        if(cells.containsKey(row)) {
+            cells.get(row).remove(column);
+        }
+    }
+
+    public Optional<Cell> getCellAt(int row, int column) {
+        if(cells.containsKey(row)) {
+            return Optional.ofNullable(cells.get(row).get(column));
+        } else {
+            return Optional.empty();
+        }
     }
 }
