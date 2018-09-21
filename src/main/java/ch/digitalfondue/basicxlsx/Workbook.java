4945b5fdaac94829d9a662b28c66de6cc4eeb424
Sylvain Jermini <sylvain.jermini@syjer.com>
2018-09-22 00:49:12 +0200

    generate xml for sheet too, add apache2 license

--- src/main/java/ch/digitalfondue/basicxlsx/Workbook.java
+++ src/main/java/ch/digitalfondue/basicxlsx/Workbook.java
@@ -7,14 +7,13 @@ import org.w3c.dom.Node;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Workbook {
+
+    public static final String NS_SPREADSHEETML_2006_MAIN = "http://schemas.openxmlformats.org/spreadsheetml/2006/main";
 
     private final Map<String, Sheet> sheets = new HashMap<>();
     private final List<String> sheetNameOrder = new ArrayList<>();
@@ -89,10 +88,10 @@ public class Workbook {
     private static String buildWorkbook(int sheetCount, List<String> sheetNameOrder) {
 
         Document doc = Utils.toDocument("ch/digitalfondue/basicxlsx/workbook_template.xml");
-        Node root = doc.getDocumentElement().getElementsByTagNameNS("http://schemas.openxmlformats.org/spreadsheetml/2006/main", "sheets").item(0);
+        Node root = doc.getDocumentElement().getElementsByTagNameNS(NS_SPREADSHEETML_2006_MAIN, "sheets").item(0);
         // <sheet name="Table0" sheetId="1" r:id="rId1"/>
         for (int i = 0; i < sheetCount; i++) {
-            Element sheet = doc.createElementNS("http://schemas.openxmlformats.org/spreadsheetml/2006/main", "sheet");
+            Element sheet = doc.createElementNS(NS_SPREADSHEETML_2006_MAIN, "sheet");
             sheet.setAttribute("name", sheetNameOrder.get(i));
             sheet.setAttribute("sheetId", Integer.toString(i + 1));
             sheet.setAttributeNS("http://schemas.openxmlformats.org/officeDocument/2006/relationships", "id", "rId" + (i + 1));
@@ -102,21 +101,42 @@ public class Workbook {
     }
 
     private static String buildSheet(Sheet sheet) {
-        System.err.println("Sheet is " + sheet.getName());
-        System.err.println("max col is " + sheet.getMaxCol());
         Document doc = Utils.toDocument("ch/digitalfondue/basicxlsx/sheet_template.xml");
-        Element cols = (Element) doc.getElementsByTagNameNS("http://schemas.openxmlformats.org/spreadsheetml/2006/main", "cols").item(0);
+        Element cols = (Element) doc.getElementsByTagNameNS(NS_SPREADSHEETML_2006_MAIN, "cols").item(0);
 
         final int colsCount = sheet.getMaxCol() + 1;
         for (int i = 0; i < colsCount; i++) {
-            Element col = doc.createElementNS("http://schemas.openxmlformats.org/spreadsheetml/2006/main", "col");
+            Element col = doc.createElementNS(NS_SPREADSHEETML_2006_MAIN, "col");
             col.setAttribute("min", Integer.toString(i + 1));
             col.setAttribute("max", Integer.toString(i + 1));
             cols.appendChild(col);
         }
 
+        Element sheetData = (Element) doc.getElementsByTagNameNS(NS_SPREADSHEETML_2006_MAIN, "sheetData").item(0);
 
-        System.err.println(Utils.fromDocument(doc));
+        for (Map.Entry<Integer, SortedMap<Integer, Cell>> rowCells : sheet.cells.entrySet()) {
+            Element row = doc.createElementNS(NS_SPREADSHEETML_2006_MAIN, "row");
+            row.setAttribute("r", Integer.toString(rowCells.getKey() + 1));
+
+            for (Map.Entry<Integer, Cell> coordAndCell : rowCells.getValue().entrySet()) {
+                // <c r="B1" t="inlineStr">
+                //  <is>
+                //    <t>Name1</t>
+                //  </is>
+                // </c>
+                Element cell = doc.createElementNS(NS_SPREADSHEETML_2006_MAIN, "c");
+                cell.setAttribute("r", Utils.fromRowColumnToExcelCoordinates(rowCells.getKey(), coordAndCell.getKey()));
+                cell.setAttribute("t", "inlineStr");
+
+                Element is = doc.createElementNS(NS_SPREADSHEETML_2006_MAIN, "is");
+                Element t = doc.createElementNS(NS_SPREADSHEETML_2006_MAIN, "t");
+                t.setTextContent(coordAndCell.getValue().value);
+                is.appendChild(t);
+                cell.appendChild(is);
+                row.appendChild(cell);
+            }
+            sheetData.appendChild(row);
+        }
         return Utils.fromDocument(doc);
     }
 }
--- src/test/java/ch/digitalfondue/basicxlsx/WorkbookTest.java
+++ src/test/java/ch/digitalfondue/basicxlsx/WorkbookTest.java
@@ -13,8 +13,13 @@ public class WorkbookTest {
 
         Sheet s = w.openSheet("test");
         s.setCellAt(new Cell("Hello"), 0, 0); //A1
+        s.setCellAt(new Cell("B1"), 0, 1);
+
+        s.setCellAt(new Cell("A2"), 1, 0);
+        s.setCellAt(new Cell("World!"), 1,1); //B2
 
         Sheet s2 = w.openSheet("test2");
+        s2.setCellAt(new Cell("Hello"), 1, 0); //A2
         s2.setCellAt(new Cell("World"), 0, 1); //B1
 
         try (FileOutputStream fos = new FileOutputStream("test.xlsx")) {
