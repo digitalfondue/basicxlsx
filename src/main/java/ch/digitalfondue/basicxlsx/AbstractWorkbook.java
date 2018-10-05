5619078b23e4051240631820df8d90cd83bde3db
Sylvain Jermini <sylvain.jermini@syjer.com>
2018-10-05 17:15:27 +0200

    refactor: initial work for supporting a StreamingWorkbook

---
+++ src/main/java/ch/digitalfondue/basicxlsx/AbstractWorkbook.java
@@ -0,0 +1,151 @@
+/**
+ * Copyright © 2018 digitalfondue (info@digitalfondue.ch)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ch.digitalfondue.basicxlsx;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import java.io.IOException;
+import java.util.*;
+import java.util.function.Function;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+class AbstractWorkbook {
+    final List<String> sheetNameOrder = new ArrayList<>();
+    final List<Style> styles = new ArrayList<>();
+    final Map<Style, Integer> styleToIdMapping = new IdentityHashMap();
+
+    int styleIdSupplier(Cell cell) {
+        if (cell.style != null) {
+            Integer r = styleToIdMapping.get(cell.style);
+            return r != null ? r : 0;
+        } else {
+            return 0;//default id
+        }
+    }
+
+
+    /**
+     * Define a new style.
+     *
+     * @return a style builder
+     */
+    public Style.StyleBuilder defineStyle() {
+        return Style.define(styles::add);
+    }
+
+    static void addFileWithDocument(ZipOutputStream zos, String fileName, Document doc) throws IOException {
+        zos.putNextEntry(new ZipEntry(fileName));
+        Utils.outputDocument(doc, zos);
+        zos.closeEntry();
+    }
+
+
+    static Element getElement(Element element, String name) {
+        return (Element) element.getElementsByTagNameNS(Utils.NS_SPREADSHEETML_2006_MAIN, name).item(0);
+    }
+
+    static Element getElement(Document doc, String name) {
+        return getElement(doc.getDocumentElement(), name);
+    }
+
+
+    private static void adjustCount(Element element, String childName) {
+        element.setAttribute("count", Integer.toString(element.getElementsByTagNameNS(Utils.NS_SPREADSHEETML_2006_MAIN, childName).getLength()));
+    }
+
+    static void writeMetadataDocuments(ZipOutputStream zos,
+                                       List<String> sheetNameOrder,
+                                       List<Style> styles, Map<Style, Integer> styleToIdMapping) throws IOException {
+        int sheetCount = sheetNameOrder.size();
+        addFileWithDocument(zos, "[Content_Types].xml", buildContentTypes(sheetCount));
+        addFileWithDocument(zos, "_rels/.rels", buildRels());
+        addFileWithDocument(zos, "xl/workbook.xml", buildWorkbook(sheetCount, sheetNameOrder));
+        addFileWithDocument(zos, "xl/styles.xml", buildStyles(styles, styleToIdMapping));
+        addFileWithDocument(zos, "xl/_rels/workbook.xml.rels", buildWorkbookRels(sheetCount));
+    }
+
+    private static Document buildContentTypes(int sheetCount) {
+        Document doc = Utils.toDocument("ch/digitalfondue/basicxlsx/content_types_template.xml");
+        Element root = doc.getDocumentElement();
+
+        // Override elements for the sheets
+        for (int i = 0; i < sheetCount; i++) {
+            // <Override PartName="/xl/worksheets/sheet1.xml" ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.worksheet+xml"/>
+            Element overrideElem = doc.createElementNS("http://schemas.openxmlformats.org/package/2006/content-types", "Override");
+            overrideElem.setAttribute("PartName", "/xl/worksheets/sheet" + (i + 1) + ".xml");
+            overrideElem.setAttribute("ContentType", "application/vnd.openxmlformats-officedocument.spreadsheetml.worksheet+xml");
+            root.appendChild(overrideElem);
+        }
+        return doc;
+    }
+
+    private static Document buildRels() {
+        return Utils.toDocument("ch/digitalfondue/basicxlsx/rels_template.xml");
+    }
+
+    private static Document buildWorkbook(int sheetCount, List<String> sheetNameOrder) {
+        Document doc = Utils.toDocument("ch/digitalfondue/basicxlsx/workbook_template.xml");
+        Element root = getElement(doc, "sheets");
+        // <sheet name="Table0" sheetId="1" r:id="rId1"/>
+        for (int i = 0; i < sheetCount; i++) {
+            Element sheet = doc.createElementNS(Utils.NS_SPREADSHEETML_2006_MAIN, "sheet");
+            sheet.setAttribute("name", sheetNameOrder.get(i));
+            sheet.setAttribute("sheetId", Integer.toString(i + 1));
+            sheet.setAttributeNS("http://schemas.openxmlformats.org/officeDocument/2006/relationships", "id", "rId" + (i + 2)); //rdId1 it's the style.xml file
+            root.appendChild(sheet);
+        }
+        return doc;
+    }
+
+    private static Document buildStyles(List<Style> styles, Map<Style, Integer> styleToIdMapping) {
+        Document doc = Utils.toDocument("ch/digitalfondue/basicxlsx/styles_template.xml");
+        Function<String, Element> elementBuilder = Utils.toElementBuilder(doc);
+
+        Element fonts = getElement(doc, "fonts");
+        Element cellXfs = getElement(doc, "cellXfs");
+        Element numFmts = getElement(doc, "numFmts");
+        Element fills = getElement(doc, "fills");
+
+        for (Style style : styles) {
+            int styleId = style.register(elementBuilder, fonts, cellXfs, numFmts, fills);
+            styleToIdMapping.put(style, styleId);
+        }
+        //
+        adjustCount(fonts, "font");
+        adjustCount(cellXfs, "xf");
+        adjustCount(numFmts, "numFmt");
+        adjustCount(fills, "fill");
+        //
+        return doc;
+    }
+
+    private static Document buildWorkbookRels(int sheetCount) {
+        Document doc = Utils.toDocument("ch/digitalfondue/basicxlsx/workbook_rels_template.xml");
+        Element root = doc.getDocumentElement();
+        // add for each sheet
+        // <Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/worksheet" Target="xl/worksheets/sheet1.xml"/>
+        for (int i = 0; i < sheetCount; i++) {
+            Element rel = doc.createElementNS("http://schemas.openxmlformats.org/package/2006/relationships", "Relationship");
+            rel.setAttribute("Id", "rId" + (i + 2)); //rdId1 it's the style.xml file
+            rel.setAttribute("Type", "http://schemas.openxmlformats.org/officeDocument/2006/relationships/worksheet");
+            rel.setAttribute("Target", "/xl/worksheets/sheet" + (i + 1) + ".xml");
+            root.appendChild(rel);
+        }
+        return doc;
+    }
+}
--- src/main/java/ch/digitalfondue/basicxlsx/Workbook.java
+++ src/main/java/ch/digitalfondue/basicxlsx/Workbook.java
@@ -23,18 +23,14 @@ import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.function.Function;
-import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 /**
  * Represent a xlsx workbook. It's the main entry point for generating a xlsx file.
  */
-public class Workbook {
+public class Workbook extends AbstractWorkbook {
 
     private final Map<String, Sheet> sheets = new HashMap<>();
-    private final List<String> sheetNameOrder = new ArrayList<>();
-    private final List<Style> styles = new ArrayList<>();
-    final Map<Style, Integer> styleToIdMapping = new IdentityHashMap();
 
     /**
      * Open or create a new sheet.
@@ -49,25 +45,6 @@ public class Workbook {
         });
     }
 
-    private int styleIdSupplier(Cell cell) {
-        if (cell.style != null) {
-            Integer r = styleToIdMapping.get(cell.style);
-            return r != null ? r : 0;
-        } else {
-            return 0;//default id
-        }
-    }
-
-
-    /**
-     * Define a new style.
-     *
-     * @return a style builder
-     */
-    public Style.StyleBuilder defineStyle() {
-        return Style.define(styles::add);
-    }
-
     /**
      * Write the current worksheet to the output stream.
      *
@@ -77,113 +54,12 @@ public class Workbook {
     public void write(OutputStream os) throws IOException {
         try (ZipOutputStream zos = new ZipOutputStream(os, StandardCharsets.UTF_8)) {
 
-            addFileWithDocument(zos, "[Content_Types].xml", buildContentTypes(sheets.size()));
-
-            addFileWithDocument(zos, "_rels/.rels", buildRels());
-
-            addFileWithDocument(zos, "xl/workbook.xml", buildWorkbook(sheets.size(), sheetNameOrder));
-
-            addFileWithDocument(zos, "xl/styles.xml", buildStyles(styles, styleToIdMapping));
-
-            addFileWithDocument(zos, "xl/_rels/workbook.xml.rels", buildWorkbookRels(sheets.size()));
+            writeMetadataDocuments(zos, sheetNameOrder, styles, styleToIdMapping);
 
             for (int i = 0; i < sheets.size(); i++) {
                 addFileWithDocument(zos, "xl/worksheets/sheet" + (i + 1) + ".xml", buildSheet(sheets.get(sheetNameOrder.get(i)), this::styleIdSupplier));
             }
         }
-    }
-
-    private static void addFileWithDocument(ZipOutputStream zos, String fileName, Document doc) throws IOException {
-        zos.putNextEntry(new ZipEntry(fileName));
-        Utils.outputDocument(doc, zos);
-        zos.closeEntry();
-    }
-
-    private static Element getElement(Element element, String name) {
-        return (Element) element.getElementsByTagNameNS(Utils.NS_SPREADSHEETML_2006_MAIN, name).item(0);
-    }
-
-    private static Element getElement(Document doc, String name) {
-        return getElement(doc.getDocumentElement(), name);
-    }
-
-    private static void adjustCount(Element element, String childName) {
-        element.setAttribute("count", Integer.toString(element.getElementsByTagNameNS(Utils.NS_SPREADSHEETML_2006_MAIN, childName).getLength()));
-    }
-
-    private static Document buildStyles(List<Style> styles, Map<Style, Integer> styleToIdMapping) {
-        Document doc = Utils.toDocument("ch/digitalfondue/basicxlsx/styles_template.xml");
-        Function<String, Element> elementBuilder = Utils.toElementBuilder(doc);
-
-        Element fonts = getElement(doc, "fonts");
-        Element cellXfs = getElement(doc, "cellXfs");
-        Element numFmts = getElement(doc, "numFmts");
-        Element fills = getElement(doc, "fills");
-
-        for (Style style : styles) {
-            int styleId = style.register(elementBuilder, fonts, cellXfs, numFmts, fills);
-            styleToIdMapping.put(style, styleId);
-        }
-
-
-        //
-        adjustCount(fonts, "font");
-        adjustCount(cellXfs, "xf");
-        adjustCount(numFmts, "numFmt");
-        adjustCount(fills, "fill");
-        //
-        return doc;
-    }
-
-    private static Document buildRels() {
-        return Utils.toDocument("ch/digitalfondue/basicxlsx/rels_template.xml");
-    }
-
-    private static Document buildContentTypes(int sheetCount) {
-
-        Document doc = Utils.toDocument("ch/digitalfondue/basicxlsx/content_types_template.xml");
-        Element root = doc.getDocumentElement();
-
-        // Override elements for the sheets
-        for (int i = 0; i < sheetCount; i++) {
-            // <Override PartName="/xl/worksheets/sheet1.xml" ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.worksheet+xml"/>
-            Element overrideElem = doc.createElementNS("http://schemas.openxmlformats.org/package/2006/content-types", "Override");
-            overrideElem.setAttribute("PartName", "/xl/worksheets/sheet" + (i + 1) + ".xml");
-            overrideElem.setAttribute("ContentType", "application/vnd.openxmlformats-officedocument.spreadsheetml.worksheet+xml");
-            root.appendChild(overrideElem);
-        }
-        return doc;
-    }
-
-    private static Document buildWorkbookRels(int sheetCount) {
-
-        Document doc = Utils.toDocument("ch/digitalfondue/basicxlsx/workbook_rels_template.xml");
-        Element root = doc.getDocumentElement();
-        // add for each sheet
-        // <Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/worksheet" Target="xl/worksheets/sheet1.xml"/>
-        for (int i = 0; i < sheetCount; i++) {
-            Element rel = doc.createElementNS("http://schemas.openxmlformats.org/package/2006/relationships", "Relationship");
-            rel.setAttribute("Id", "rId" + (i + 2)); //rdId1 it's the style.xml file
-            rel.setAttribute("Type", "http://schemas.openxmlformats.org/officeDocument/2006/relationships/worksheet");
-            rel.setAttribute("Target", "/xl/worksheets/sheet" + (i + 1) + ".xml");
-            root.appendChild(rel);
-        }
-        return doc;
-    }
-
-    private static Document buildWorkbook(int sheetCount, List<String> sheetNameOrder) {
-
-        Document doc = Utils.toDocument("ch/digitalfondue/basicxlsx/workbook_template.xml");
-        Element root = getElement(doc, "sheets");
-        // <sheet name="Table0" sheetId="1" r:id="rId1"/>
-        for (int i = 0; i < sheetCount; i++) {
-            Element sheet = doc.createElementNS(Utils.NS_SPREADSHEETML_2006_MAIN, "sheet");
-            sheet.setAttribute("name", sheetNameOrder.get(i));
-            sheet.setAttribute("sheetId", Integer.toString(i + 1));
-            sheet.setAttributeNS("http://schemas.openxmlformats.org/officeDocument/2006/relationships", "id", "rId" + (i + 2)); //rdId1 it's the style.xml file
-            root.appendChild(sheet);
-        }
-        return doc;
     }
 
     private static Document buildSheet(Sheet sheet, Function<Cell, Integer> styleIdSupplier) {
